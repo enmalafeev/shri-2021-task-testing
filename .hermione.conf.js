262421f5ebe26b67c1d71b33ae01960843eab678
enmalafeev <malafeev.en@yandex.ru>
2021-10-23 20:59:04 +0300

    comment html-reporter

--- .hermione.conf.js
+++ .hermione.conf.js
@@ -13,9 +13,9 @@ module.exports = {
     width: 575,
     height: 1000
   },
-  plugins: {
-    'html-reporter/hermione': {
-      path: 'hermione-html-reporter'
-    }
-  }
+  // plugins: {
+  //   'html-reporter/hermione': {
+  //     path: 'hermione-html-reporter'
+  //   }
+  // }
 }
