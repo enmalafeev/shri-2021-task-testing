61b94df407d0c4acde579ae0eeb90ee9117dfa8c
enmalafeev <malafeev.en@yandex.ru>
2021-10-23 20:57:34 +0300

    change Product.test

--- test/unit/pages/Product.test.tsx
+++ test/unit/pages/Product.test.tsx
@@ -1,4 +1,4 @@
-import { screen, render } from '@testing-library/react';
+import { screen, render, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import * as redux from 'react-redux';
 import React from 'react';
@@ -7,6 +7,7 @@ import { Product } from '../../../src/client/pages/Product';
 import { CartApi, ExampleApi } from '../../../src/client/api';
 import { initStore } from '../../../src/client/store';
 import { BrowserRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 
 describe('Проверка карточки товара', () => {
   const spy = jest.spyOn(redux, 'useSelector');
@@ -24,19 +25,44 @@ describe('Проверка карточки товара', () => {
   const cart = new CartApi();
   const store = initStore(api, cart);
 
-  it('Shows info about product', () => {
-    render(
+  it('Показать информацию о товаре', () => {
+    const product = (
       <Provider store={store}>
         <BrowserRouter>
           <Product />
         </BrowserRouter>
       </Provider>
     );
+
+    const { getByRole, getByText, container} = render(product);
+
+    const color = container.querySelector('.ProductDetails-Color').innerHTML;
+    const material = container.querySelector('.ProductDetails-Material').innerHTML;
     
-    expect(screen.getByRole('heading', { name: /notebook/i })).toBeInTheDocument();
-    expect(screen.getByText(/some description about the product/i)).toBeInTheDocument();
-    expect(screen.getByText(/color/i)).toBeInTheDocument();
-    expect(screen.getByText(/material/i)).toBeInTheDocument();
-    expect(screen.getByRole('button', { name: /add to cart/i })).toBeInTheDocument();
+    expect(getByRole('heading', { name: /notebook/i })).toBeInTheDocument();
+    expect(getByText(/some description about the product/i)).toBeInTheDocument();
+    expect(getByText(/\$300/i)).toBeInTheDocument();
+    expect(getByRole('button', { name: /add to cart/i })).toBeInTheDocument();
+    expect(getByText(/color/i)).toBeInTheDocument();
+    expect(color).toEqual('grey');
+    expect(getByText(/material/i)).toBeInTheDocument();
+    expect(material).toEqual('steel');
   });
+
+  it('При клике на добавить в Корзину показывается информация о добавлении', () => {
+    const product = (
+      <Provider store={store}>
+        <BrowserRouter>
+          <Product />
+        </BrowserRouter>
+      </Provider>
+    );
+
+    const { getByRole, getByText } = render(product);
+
+    userEvent.click(getByRole('button', { name: /add to cart/i }));
+    waitFor(() => expect(getByText('Item in cart')).toBeInTheDocument());
+    
+    screen.logTestingPlaygroundURL();
+  })
 });
