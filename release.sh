3560ce51f94691b317204aa8335b965883ced39e
enmalafeev <malafeev.en@yandex.ru>
2021-10-30 20:47:46 +0300

    feat: add release.sh script

--- release.sh
+++ release.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-echo Hello world!
+export LAST_RELEASE_TAG=$(git describe)
+export DIFF_BETWEEN_LAST_TAGS=$(git log $(git describe --abbrev=0 --tags $(git describe --abbrev=0)^)...$(git describe --abbrev=0))
+
+echo "${DIFF_BETWEEN_LAST_TAGS}"
